dbdd609581ae03dfda942fd834b69274b637db23
Evelyn <evelynzanpx@gmail.com>
2024-09-12 20:44:31 +0700

    add m01-task2

---
+++ Week 1 - Introduction to OOP/m01-task2.py
@@ -0,0 +1,36 @@
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class Produk:
+    def __init__(self, nama, stok):
+        self.nama = nama
+        self.stok = stok
+
+
+barang = ["Hand Sanitizer", "Ban Mobil", "Buah-Buahan", "Botol Minuman Bayi"]
+daftar_produk = []
+
+clear_screen()
+
+print(f"{"PENGINPUTAN STOK BARANG":^50}")
+print("=" * 50)
+
+for i in range(len(barang)):
+    stok = int(input(f"Masukkan jumlah stok {barang[i]}: "))
+    produk = Produk(barang[i], stok)
+    daftar_produk.append(produk)
+
+print()
+
+print(f"{"LAPORAN STOK BARANG":^50}")
+print("=" * 50)
+
+print(f"{"Nama Produk":<30}{"Jumlah Stok"}")
+print("-" * 50)
+
+for i in range(len(daftar_produk)):
+    print(f"{daftar_produk[i].nama:<30}{daftar_produk[i].stok}")
+
+print()
