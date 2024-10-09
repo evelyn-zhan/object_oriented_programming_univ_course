19d084a7c5a66d996a7d5f9d77759b8e202784a0
Evelyn <evelynzanpx@gmail.com>
2024-10-09 10:14:30 +0700

    update m02-task2

--- Week 02 - Inheritance and Polymorphism/m02-task2.py
+++ Week 02 - Inheritance and Polymorphism/m02-task2.py
@@ -4,66 +4,67 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 class Produk:
-    def __init__(self, nama, stok, harga_beli, harga_jual):
+    def __init__(self, id, nama, stok, harga):
+        self.id = id
         self.nama = nama
         self.stok = stok
-        self.harga_beli = harga_beli
-        self.harga_jual = harga_jual
+        self.harga = harga
         self.pembelian = 0
-    
+        self.penjualan = 0
+
 class HandSanitizer(Produk):
-    def __init__(self, nama, stok, harga_beli, harga_jual, ml):
-        super().__init__(nama, stok, harga_beli, harga_jual)
+    def __init__(self, id, nama, stok, harga, ml):
+        super().__init__(id, nama, stok, harga)
         self.ml = ml
 
 class BanMobil(Produk):
-    def __init__(self, nama, stok, harga_beli, harga_jual, diameter):
-        super().__init__(nama, stok, harga_beli, harga_jual)
+    def __init__(self, id, nama, stok, harga, diameter):
+        super().__init__(id, nama, stok, harga)
         self.diameter = diameter
 
 class BuahBuahan(Produk):
-    def __init__(self, nama, stok, harga_beli, harga_jual, berat):
-        super().__init__(nama, stok, harga_beli, harga_jual)
+    def __init__(self, id, nama, stok, harga, berat):
+        super().__init__(id, nama, stok, harga)
         self.berat = berat
 
 class BotolMinumanBayi(Produk):
-    def __init__(self, nama, stok, harga_beli, harga_jual, ukuran):
-        super().__init__(nama, stok, harga_beli, harga_jual)
+    def __init__(self, id, nama, stok, harga, ukuran):
+        super().__init__(id, nama, stok, harga)
         self.ukuran = ukuran
 
+id = ["ITM-001", "ITM-002", "ITM-003", "ITM-004", "ITM-005"]
 barang = ["Hand Sanitizer", "Ban Mobil", "Buah-Buahan", "Botol Minuman Bayi"]
-harga_beli = [25000, 100000, 70000, 30000]
-harga_jual = [30000, 120000, 80000, 40000]
+harga = [25000, 100000, 70000, 30000]
 stok = [5, 3, 4, 3]
 produk = []
 
-produk.append(HandSanitizer("Hand Sanitizer", stok[0], harga_beli[0], harga_jual[0], "150 ml"))
-produk.append(BanMobil("Ban Mobil", stok[1], harga_beli[1], harga_jual[1], "60 cm"))
-produk.append(BuahBuahan("Buah-Buahan", stok[2], harga_beli[2], harga_jual[2], "1 kg"))
-produk.append(BotolMinumanBayi("Botol Minuman Bayi", stok[3], harga_beli[3], harga_jual[3], "250 ml"))
+produk.append(HandSanitizer(id[0], barang[0], stok[0], harga[0], "150 ml"))
+produk.append(BanMobil(id[1], barang[1], stok[1], harga[1], "60 cm"))
+produk.append(BuahBuahan(id[2], barang[2], stok[2], harga[2], "1 kg"))
+produk.append(BotolMinumanBayi(id[3], barang[3], stok[3], harga[3], "250 ml"))
 
-def cek_stok():
-    print(f"{"CEK STOK BARANG":^70}")
-    print("=" * 70)
+def tampilkan_barang():
+    print(f"{"TAMPILKAN BARANG":^80}")
+    print("=" * 80)
 
-    print(f"{"No":<5}{"Nama Barang":<25}{"Stok"}")
+    print(f"{"No":<5}{"ID":<10}{"Nama Barang":<25}{"Stok":<8}{"Pembelian":<12}{"Penjualan"}")
 
     for i in range(len(produk)):
-        print(f"{i + 1:<5}{produk[i].nama:<25}{produk[i].stok}")
+        print(f"{i + 1:<5}{produk[i].id:<10}{produk[i].nama:<25}{produk[i].stok:<8}{produk[i].pembelian:<12}{produk[i].penjualan}")
     
-    print("=" * 70)
+    print("=" * 80)
     input("Tekan ENTER untuk kembali...")
 
-def jual_produk():
-    print(f"{"JUAL PRODUK":^70}")
-    print("=" * 70)
+def jual_barang():
+    print(f"{"JUAL PRODUK":^80}")
+    print("=" * 80)
 
-    print(f"{"No":<5}{"Produk":<25}{"Stok":<8}{"Harga Jual"}")
+    print(f"{"No":<5}{"ID":<10}{"Produk":<25}{"Stok"}")
 
     for i in range(len(produk)):
-        print(f"{i + 1:<5}{produk[i].nama:<25}{produk[i].stok:<8}{produk[i].harga_jual}")
+        print(f"{i + 1:<5}{produk[i].id:<10}{produk[i].nama:<25}{produk[i].stok}")
     
-    print("=" * 70)
+    print("=" * 80)
 
     nomor = int(input("Masukkan nomor barang yang ingin dijual: "))
 
@@ -82,89 +83,110 @@ def jual_produk():
         jumlah = int(input(f"Masukkan jumlah {produk[nomor].nama} yang ingin dijual: "))
     
     produk[nomor].stok -= jumlah
+    produk[nomor].penjualan += jumlah
 
-    print("=" * 70)
+    print("=" * 80)
     print(f"{produk[nomor].nama} telah dijual sebanyak {jumlah} buah.")
     input("Tekan ENTER untuk kembali...")
 
-def beli_stok():
-    print(f"{"BELI STOK BARANG":^70}")
-    print("=" * 70)
+def tambah_barang():
+    print(f"{"TAMBAH BARANG":^80}")
+    print("=" * 80)
+    print("1. Beli Barang Baru")
+    print("2. Tambah Stok Barang Lama")
+    print("=" * 80)
 
-    print(f"{"No":<5}{"Produk":<25}{"Harga Beli"}")
-
-    for i in range(len(produk)):
-        print(f"{i + 1:<5}{produk[i].nama:<25}{produk[i].harga_beli}")
-    
-    print("=" * 70)
-
-    nomor = int(input("Masukkan nomor barang yang ingin dibeli: "))
-
-    while nomor < 1 or nomor > len(produk):
-        print("Tidak ada produk dengan nomor tersebut.\n")
-        nomor = int(input("Masukkan nomor barang yang ingin dibeli: "))
-    
-    nomor -= 1
+    pilihan = int(input("Masukkan pilihan [1/2]: "))
 
     print()
 
-    jumlah = int(input(f"Masukkan berapa buah {produk[nomor].nama} yang ingin dibeli: "))
+    if pilihan == 1:
+        id = input(f"{"ID Barang":<20}: ").upper()
+        nama = input(f"{"Nama Barang":<20}: ").title()
+        harga_beli = int(input(f"{"Harga Beli":<20}: "))
+        stok = int(input(f"{"Jumlah Pembelian":<20}: "))
 
-    while jumlah <= 0:
-        print("Jumlah barang yang dibeli minimal 1.\n")
-        jumlah = int(input(f"Masukkan berapa buah {produk[nomor].nama} yang ingin dijumlah: "))
+        produk_baru = Produk(id, nama, stok, harga_beli)
+        produk_baru.pembelian = stok
+        produk.append(produk_baru)
 
-    produk[nomor].stok += jumlah
-    produk[nomor].pembelian += jumlah
+        print("=" * 80)
+        print("Barang berhasil ditambahkan")
+    elif pilihan == 2:
+        print(f"{"No":<5}{"ID":<10}{"Nama Barang":<25}{"Harga":<10}{"Stok"}")
 
-    print("=" * 70)
-    print(f"{produk[nomor].nama} telah dibeli sebanyak {jumlah} buah.")
+        for i in range(len(produk)):
+            print(f"{i + 1:<5}{produk[i].id:<10}{produk[i].nama:<25}{produk[i].harga:<10}{produk[i].stok}")
+        
+        print()
+
+        nomor = int(input("Masukkan nomor barang yang ingin dibeli: "))
+
+        while nomor < 1 or nomor > len(produk):
+            print("Tidak ada produk dengan nomor tersebut.\n")
+            nomor = int(input("Masukkan nomor barang yang ingin dibeli: "))
+        
+        nomor -= 1
+
+        print()
+
+        jumlah = int(input(f"Masukkan jumlah {produk[nomor].nama} yang ingin dibeli: "))
+
+        while jumlah <= 0:
+            print("Jumlah barang yang dibeli minimal 1.\n")
+            jumlah = int(input(f"Masukkan jumlah {produk[nomor].nama} yang ingin dibeli: "))
+
+        produk[nomor].stok += jumlah
+        produk[nomor].pembelian += jumlah
+
+        print("=" * 80)
+        print("Barang berhasil ditambahkan")
+    else:
+        print("Pilihan tidak valid")
+
     input("Tekan ENTER untuk kembali...")
 
-def laporan_pengeluaran():
-    print(f"{"LAPORAN PENGELUARAN":^70}")
-    print("=" * 70)
+def laporan():
+    print(f"{"LAPORAN PENGELUARAN":^80}")
+    print("=" * 80)
 
     total = 0
 
-    print(f"{"No":<5}{"Produk":<25}{"Harga Beli":<15}{"Jumlah Pembelian"}")
+    print(f"{"ID":<10}{"Nama Barang":<25}{"Harga":<10}{"Stok":<10}{"Pembelian":<12}{"Penjualan"}")
 
     for i in range(len(produk)):
-        print(f"{i + 1:<5}{produk[i].nama:<25}{produk[i].harga_beli:<15}{produk[i].pembelian}")
-        total += produk[i].pembelian * produk[i].harga_beli
+        print(f"{produk[i].id:<10}{produk[i].nama:<25}{produk[i].harga:<10}{produk[i].stok:<10}{produk[i].pembelian:<12}{produk[i].penjualan}")
+        total += produk[i].pembelian * produk[i].harga
     
-    print("=" * 70)
+    print("=" * 80)
     print(f"Total Pengeluaran: Rp {total}")
     input("Tekan ENTER untuk kembali...")
 
 while True:
     clear_screen()
 
-    print(f"{"MENU":^70}")
-    print("=" * 70)
-    print("1. Cek Stok")
-    print("2. Jual Produk")
-    print("3. Beli Stok")
-    print("4. Laporan Pengeluaran")
-    print("5. Keluar")
-    print("=" * 70)
+    print(f"{"MENU":^80}")
+    print("=" * 80)
+    print("1. Tambah Barang")
+    print("2. Jual Barang")
+    print("3. Tampilkan Barang / Laporan")
+    print("4. Keluar")
+    print("=" * 80)
 
-    pilihan = int(input("\nPilih menu [1/2/3/4/5]: "))
+    pilihan = int(input("Pilih menu [1/2/3/4]: "))
 
-    if 1 <= pilihan <= 4:
+    if 1 <= pilihan <= 3:
         clear_screen()
 
     if pilihan == 1:
-        cek_stok()
+        tambah_barang()
     elif pilihan == 2:
-        jual_produk()
+        jual_barang()
     elif pilihan == 3:
-        beli_stok()
+        laporan()
     elif pilihan == 4:
-        laporan_pengeluaran()
-    elif pilihan == 5:
-        print("\nTerima kasih.\n")
+        print("Terima kasih.\n")
         break
     else:
-        print("\nPilihan tidak valid.")
+        print("Pilihan tidak valid.")
         input("Tekan ENTER untuk kembali...")
