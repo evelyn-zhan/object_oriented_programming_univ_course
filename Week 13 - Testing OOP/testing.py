a4d983a0855b88d935ae759675b7c91b0286e284
Evelyn <evelynzanpx@gmail.com>
2024-12-19 10:32:14 +0700

    Add m13-task1

---
+++ Week 13 - Testing OOP/testing.py
@@ -0,0 +1,20 @@
+import unittest
+from m13task1 import DaftarMahasiswa, DaftarDosen, Mahasiswa, Dosen
+
+df_mhs = DaftarMahasiswa()
+df_dsn = DaftarDosen()
+
+df_mhs.tambah(Mahasiswa('231111684', 'Evelyn', 'Perempuan', '08123456789', 'Teknik Informatika'))
+
+class test_program(unittest.TestCase):
+    def test_jumlah(self):
+        self.assertEqual(df_mhs.jumlah(), 1)
+    
+    def test_data(self):
+        self.assertEqual(df_mhs.isi[0].nim, '231111684')
+    
+    def test_salah(self):
+        self.assertEqual(df_dsn.jumlah(), 2)
+
+if __name__ == '__main__':
+    unittest.main()
